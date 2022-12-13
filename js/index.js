2b9edfadd0cb785c402947c308cbbe427f44622e
RasulUbaev <ubaevrasul@gmail.com>
2022-12-13 09:59:03 +0500

    aaaa

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var $burger = document.getElementById("burger");
 var $menu = document.getElementById ("menu");
 var $menux =document.getElementsByClassName(`menux`);
-
+var header=document.querySelector (`header`);
 
 
 $burger.addEventListener(`click`, function () {
     $menu.classList.toggle(`-translate-x-full`);
 
 })
+
 
